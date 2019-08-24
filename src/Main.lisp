f427180d15caf4c4e865db70e564a559e8fd4481
kantarou <s1190064@gmail.com>
2019-08-25 04:19:29 +0900

    add graund

--- src/Main.lisp
+++ src/Main.lisp
@@ -2,7 +2,27 @@
 (load "src/Vector3D.fasl")
 (load "src/Ray.fasl")
 (load "src/Sphere.fasl")
+(load "src/Hitable.fasl")
 (load "src/HitableList.fasl")
+
+
+(defmethod get-color ((ray Ray)
+		      (world HitableList))
+
+  (let ((rec (make-instance 'HitRecord)))
+    (if (hit ray world rec 0.0 sb-ext:double-float-positive-infinity)
+	(v* 0.5
+	   (v+ (hit-recode-normal rec) #(1 1 1)))
+	(progn
+	  (let* ((unit-direction (v-unit (ray-direction ray)))
+		(temp (* 0.5
+			 (+ (aref unit-direction 1) 1))))
+	    (v+ (v* (- 1 temp)
+		  #(1 1 1))
+	       (v* temp
+		   #(0.5 0.7 1.0))))))))
+
+  
 
 (defun main ()
   (let (sky lower-left-corner horizontal vertical origin list world)
@@ -10,9 +30,9 @@
     (setf lower-left-corner #(-2.0 -1.0 -1.0))
     (setf horizontal #(4.0 0.0 0.0))
     (setf vertical   #(0.0 2.0 0.0))
-    (setf origin	 #(0.0 0.0 0.0))
-    (setf list `((make-instance 'Sphere :center #(0 0 -1) :radius 0.5)
-		 (make-instance 'Sphere :center #(0 -100.5 -1) :radius 100)))
+    (setf origin     #(0.0 0.0 0.0))
+    (setf list `(,(make-instance 'Sphere :center #(0 0 -1) :radius 0.5)
+		 ,(make-instance 'Sphere :center #(0 -100.5 -1) :radius 100)))
     (setf world (make-instance 'HitableList :list-size 2 :list list))
     
     
@@ -25,13 +45,14 @@
 		 
 		 (r (make-instance 'ray :origine origin
 				   :direction (v+ (v+ lower-left-corner (v* u horizontal)) (v* v vertical))))
-		 (p (point-at-parameter 2.0 r))
+		 ;(p (point-at-parameter 2.0 r))
 
-		 (col
-		  (transform-ray-to-color r)))
+		 (col (make-instance 'Color :color (get-color r world))))
+					;(transform-ray-to-color r)))
 
-	    ;;transform color value to RGB
+		 ;;transform color value to RGB
 	    (setf (color-array col)
-		  (map 'vector #'(lambda (x) (floor (* x 255.99))) (color-array col)))
-	    (set-image-color sky x y col)))))
+		   (map 'vector #'(lambda (x) (floor (* x 255.99))) (color-array col)))
+		 (set-image-color sky x y col)))))
     (save-image-ppm "sky" sky)))
+
